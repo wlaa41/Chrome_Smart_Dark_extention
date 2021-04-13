0f9408bfeab92697195eeb1a0d1a04cc6ba7b8af
Will <WLAA41@GMAIL.COM>
2021-04-14 00:14:27 +0100

    bug free version

--- setdarkmood copy.js
+++ setdarkmood copy.js
@@ -1,39 +1,38 @@
+let DomElem = 0;
+console.log('mutation');
+
 
 // this to stop updating at every DOM update as -addEventListener DOMNodeInserted- fires a lot in the begining
 let updateNow = false  
 time2wait = 3000 // the time start slow then it become faster to be more responsive
-ripitition_counter=0;
+repetition_counter=0;
 let restartcounter = true
-let repitiionAllowed = 100
+let repetitionAllowed = 300
 
 
-holdrepititiveUpdate('first call')
+// holdrepititiveUpdate('first call')
 
 
 function updateifActive(){
     chrome.storage.sync.get("active", ({ active }) => {
-        // changeColor.style.backgroundColor = color;
-        // changeColor.innerHTML=""+active
-        // console.log('hello ################################3')
-        console.log('this is the event')
-        console.log(event);
-        console.log(active)
-        if(active){setPageBackgroundColor()}
+
+
+        if(active){
+            setPageBackgroundColor()
+            document.addEventListener('DOMNodeInserted', holdrepititiveUpdate);
+        }
+
     })
 }
 
 updateifActive()// first start up ....
 
 function nodeInsertedCallback(event) {
-    console.log(updateNow + '  updating ')
-    if(updateNow ){
-
-    // updateOnHold  = false
-    // console.log('updateOnHold is set to '+updateOnHold)
-    updateifActive()
+    // console.log(updateNow + '  updating ')
+    if(true ){
+         updateifActive()
     }
     else{ 
-        // updateOnHold  = true
         console.log('No update yet')
     };
 };
@@ -42,186 +41,170 @@ function nodeInsertedCallback(event) {
 // let updateOnHold  = true
 function holdrepititiveUpdate(event){
 
-        ripitition_counter+=1
-        console.log('it is ok counter at '+ ripitition_counter)
-        
-
-        if(ripitition_counter < repitiionAllowed )
+        repetition_counter+=1
+        console.log('Repetition counter at ' , repetition_counter)
+        if(repetition_counter < repetitionAllowed )
         {           
 
             nodeInsertedCallback(event) 
+            // console.log(repetition_counter)
 
             if(restartcounter)
             {
-                restart = false
+                restartcounter = false
                 setTimeout(() => {            
-                    ripitition_counter = 0
-                    console.log('restarting counter '+ ripitition_counter)
+                    repetition_counter = 0
+                    console.log('restarting counter '+ repetition_counter)
                     restartcounter = true
-                    repitiionAllowed =5 
+                    repetitionAllowed =5 
+                    console.log('%cStart Lisening',"color: green")
+                    document.addEventListener('DOMNodeInserted', holdrepititiveUpdate);
+
                 }, 2000);
             }
             
         }
         else
         {
+            console.log('%cStop Lisening',"color: red")
+            document.removeEventListener('DOMNodeInserted',holdrepititiveUpdate)
             console.log('too many updates')
         }
 }
 
-// setInterval(()=>{
-    
-//     console.log('chnging updateNow to ',!updateNow)
-//     if(updateNow){
-//         if(updateOnHold){
-//             nodeInsertedCallback(" * waiting if active ") 
-//             updateOnHold  = false
-//             console.log('updateOnHold is set to '+updateOnHold)
-//          }
-
-//     }
-//     else{
-
-//     }
-//     updateNow = !updateNow
-// },time2wait)
-
-document.addEventListener('DOMNodeInserted', holdrepititiveUpdate);
 
 
 function setPageBackgroundColor() {
 
-    chrome.storage.sync.get("color", ({ color }) => {
-
-    function rgbatotext(rgba_array){
-        [r,g,b,a]=rgba_array
-        return "rgba("+r+","+g+","+b+","+a+ ")";
-    }
-    
-    // dark or bright this method flips the value with keeping the color degree
-    function flipColor(rgba_arry,rate=2){
-
-        // if(rate==1){console.log(rate+" "+rgba_arry+ 'before %c#######'  , `color: ${rgbatotext(rgba_arry)}`);}
-        let min = Math.min(...rgba_arry.slice(0,3))
-        let max = Math.max(...rgba_arry.slice(0,3)) 
-        for (let i = 0; i < rgba_arry.length-1; i++) {
-            rgba_arry[i] = (255-max- min + parseFloat(rgba_arry[i]) ) * rate;
-        }
-        let after = rgba_arry
-        // if(rate==1){console.log(after + 'after %cback  #########' + min , `color: ${rgbatotext(after)}`)}
-        return after
-    }
-
-    
-    
-
-    // let e = document.getElementsByClassName("smart-bar--smart-bar--yellow--3RXTf")
-    // console.log('e which the yollow ')
-    // console.log('----')
-    // console.log(e)
-    // Dynamic Operator     smart-bar--smart-bar--yellow--3RXTfsmart-bar--smart-bar--32jNQ smart-bar--smart-bar--yellow--3RXTf
-    var operator_table = {
-        moreThan: function(a, b) { return a > b; },
-        lessThan: function(a, b) { return a < b; }
-    };
-
-    // the properties to be changed in the dom element
-    let properties = [{ name: "background-color", ratio: 2, camelCase: "backgroundColor"  , 
-                            element: null, threshold:450,compare: operator_table.moreThan, missingValue_replace: "rgb(130,120,150)"},
-                        { name: "color", ratio: 1, camelCase: "color" , element: null, 
-                            threshold:350,compare: operator_table.lessThan, missingValue_replace: "rgba(255,120,150,1)"},
-                        { name: "border_color", ratio: 1, camelCase: "borderColor"  ,
-                            element: null, threshold:350,compare: operator_table.lessThan, missingValue_replace: "rgb(130,120,150)"} ,
-                        { name: "box-shadow", ratio: 1, camelCase: "boxShadow"  ,
-                            element: null, threshold:400,compare: operator_table.lessThan, missingValue_replace: "rgb(130,120,150)"} ]
-
-
-
     let elem =   document.body.getElementsByTagName("*");
 
-    checkChange(document.getElementsByTagName('body')[0])// this to check the body as the it was not included in the main wildselector
-    for (var i = 0; i < elem.length; i++) { 
-        checkChange(elem[i])
-    }
 
-
-    console.log('number of element in the DOM '+ elem.length)
-    console.log('element number 7 in the DOM  '+ elem.length)
-    console.log(elem[6])
-
-    function checkChange(element)
+    if(elem.length!== DomElem) // checking if the dom element has chaged to perform a change
     {
-        for (let j = 0; j < properties.length; j++) {
+            DomElem= elem.length
+            chrome.storage.sync.get("color", ({ color }) => {
 
-            properties[j].element = window.getComputedStyle(element, null).getPropertyValue(properties[j].name);
-         
-
-            // handling missing data
-            var rgba = properties[j].element  ?properties[j].element: properties[j].missingValue_replace ; 
-            if(element.classList.contains("udlite-badge-bestseller") ||
-              rgba === "rga(255, 231, 153)"){
-
-                console.log('catch it     #############################################3')
-                console.log(element)
+            function rgbatotext(rgba_array){
+                [r,g,b,a]=rgba_array
+                return "rgba("+r+","+g+","+b+","+a+ ")";
             }
-            // console.log(rgba,properties[j].name) 
-        
-            if(rgba !== undefined && rgba.length > 5) {
+            
+            // dark or bright this method flips the value with keeping the color degree
+            function flipColor(rgba_arry,rate=2){
 
-                    rgba = rgba.split('(')[1].split(')')[0]
-                            .replace(/ /g, '')
-                            .split(',');
-
-                    // convert to number
-                    rgba= rgba.map(Number) 
-                    
-                    if (rgba[0]+rgba[1]+rgba[2] === 639) { // this methon was used to catch amazon wrong transparency behavior
-                        console.log(rgbatotext(rgba) + "  639  "+  properties[j].name +"   "+ element)
-                        console.log('%c  sum   '+   sum_bckgrnd  +'   ##' ,`color: lightgreen`)
-                    }
-                    if(rgba.length<4){
-                        rgba = rgba.concat([1]) // changing rgb to rgba
-                    }
-       
-
-                    // Cancel transparency 
-                    rgba[3]= ( rgba[3] < 0.2 )? .2:rgba[3]
-
-                
-                    const sum_bckgrnd= rgba[0]+rgba[1]+rgba[2] // calculate how bright the element is. the larger the number is the brighter it is
-                    
-                    if (sum_bckgrnd === 639) { // this methon was used to catch amazon wrong transparency behavior
-                        console.log(rgbatotext(rgba) + "  639  "+  properties[j].name +"   "+ element)
-                        console.log('%c  sum   '+   sum_bckgrnd  +'   ##' ,`color: lightgreen`)
-                    }
-
-                    threshold = properties[j].threshold  // the threshole where change in the color will happen
-                    let newbackground = rgba
-                    if (properties[j].compare(sum_bckgrnd,threshold)) {   // Dynamic comparision as some properties must have dark and other bright change
-                        let newbackground = flipColor(rgba, properties[j].ratio) // calculating the new color
-                    }
-                        let name = properties[j].name
-
-                        element.style.removeProperty(name);
-
-                        element.style.setProperty(name, ""+rgbatotext(newbackground)+"", "important");
-                
-
-                           
-                        if (sum_bckgrnd === 639)  { // this methon was used to catch amazon wrong transparency behavior
-                            console.log(rgbatotext(newbackground) + "  "+  properties[j].name)
-                            console.log('##########   after   ###############')
-                        }
-                   
+                // if(rate==1){console.log(rate+" "+rgba_arry+ 'before %c#######'  , `color: ${rgbatotext(rgba_arry)}`);}
+                let min = Math.min(...rgba_arry.slice(0,3))
+                let max = Math.max(...rgba_arry.slice(0,3)) 
+                for (let i = 0; i < rgba_arry.length-1; i++) {
+                    rgba_arry[i] = (255-max- min + parseFloat(rgba_arry[i]) ) * rate;
+                }
+                let after = rgba_arry
+                // if(rate==1){console.log(after + 'after %cback  #########' + min , `color: ${rgbatotext(after)}`)}
+                return after
             }
+
+            var operator_table = {
+                moreThan: function(a, b) { return a > b; },
+                lessThan: function(a, b) { return a < b; }
+                };
+
+            // the properties to be changed in the dom element
+            let properties = [{ name: "background-color", ratio: 2, camelCase: "backgroundColor"  , 
+                                    element: null, threshold:450,compare: operator_table.moreThan, missingValue_replace: "rgb(130,120,150)"},
+                                { name: "color", ratio: 1, camelCase: "color" , element: null, 
+                                    threshold:350,compare: operator_table.lessThan, missingValue_replace: "rgba(255,120,150,1)"},
+                                { name: "border_color", ratio: 1, camelCase: "borderColor"  ,
+                                    element: null, threshold:350,compare: operator_table.lessThan, missingValue_replace: "rgb(130,120,150)"} ,
+                                { name: "box-shadow", ratio: 1, camelCase: "boxShadow"  ,
+                                    element: null, threshold:400,compare: operator_table.lessThan, missingValue_replace: "rgb(130,120,150)"} ]
+
+
+
+
+            checkChange(document.getElementsByTagName('body')[0])// this to check the body as the it was not included in the main wildselector
+            for (var i = 0; i < elem.length; i++) { 
+                checkChange(elem[i])
+            }
+
+
+            console.log('%cChanged detected number of element in the DOM '+ elem.length, "color:lightcoral")
+            // console.log('element number 7 in the DOM  '+ elem.length)
+            // console.log(elem[6])
+
+            function checkChange(element)
+            {
+                for (let j = 0; j < properties.length; j++) {
+
+                    properties[j].element = window.getComputedStyle(element, null).getPropertyValue(properties[j].name);
                 
-        }
-    }
 
+                    // handling missing data
+                    var rgba = properties[j].element  ?properties[j].element: properties[j].missingValue_replace ; 
+                    if(element.classList.contains("udlite-badge-bestseller") ||
+                    rgba === "rga(255, 231, 153)"){
 
+                        console.log('catch it     #############################################3')
+                        console.log(element)
+                    }
+                    // console.log(rgba,properties[j].name) 
+                
+                    if(rgba !== undefined && rgba.length > 5) {
 
-});
+                            rgba = rgba.split('(')[1].split(')')[0]
+                                    .replace(/ /g, '')
+                                    .split(',');
 
+                            // convert to number
+                            rgba= rgba.map(Number) 
+                            
+                            if (rgba[0]+rgba[1]+rgba[2] === 639) { // this methon was used to catch amazon wrong transparency behavior
+                                console.log(rgbatotext(rgba) + "  639  "+  properties[j].name +"   "+ element)
+                                console.log('%c  sum   '+   sum_bckgrnd  +'   ##' ,`color: lightgreen`)
+                            }
+                            if(rgba.length<4){
+                                rgba = rgba.concat([1]) // changing rgb to rgba
+                            }
+            
+
+                            // Cancel transparency 
+                            rgba[3]= ( rgba[3] < 0.2 )? .2:rgba[3]
+
+                        
+                            const sum_bckgrnd= rgba[0]+rgba[1]+rgba[2] // calculate how bright the element is. the larger the number is the brighter it is
+                            
+                            if (sum_bckgrnd === 639) { // this methon was used to catch amazon wrong transparency behavior
+                                console.log(rgbatotext(rgba) + "  639  "+  properties[j].name +"   "+ element)
+                                console.log('%c  sum   '+   sum_bckgrnd  +'   ##' ,`color: lightgreen`)
+                            }
+
+                            threshold = properties[j].threshold  // the threshole where change in the color will happen
+                            let newbackground = rgba
+                            if (properties[j].compare(sum_bckgrnd,threshold)) {   // Dynamic comparision as some properties must have dark and other bright change
+                                let newbackground = flipColor(rgba, properties[j].ratio) // calculating the new color
+                            }
+                                let name = properties[j].name
+
+                                element.style.removeProperty(name);
+
+                                element.style.setProperty(name, ""+rgbatotext(newbackground)+"", "important");
+                        
+
+                                
+                                if (sum_bckgrnd === 639)  { // this methon was used to catch amazon wrong transparency behavior
+                                    console.log(rgbatotext(newbackground) + "  "+  properties[j].name)
+                                    console.log('##########   after   ###############')
+                                }
+                        
+                    }
+                        
+                }
+            }
+                });
+
+    } 
+    else{
+        console.log('%cNo change detected on the dom number of element :'  + elem.length, "color:lightblue")
+    }  
 }
 
 chrome.storage.sync.get("active", ({ active }) => {
